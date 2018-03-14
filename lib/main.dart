9cdda6f51938ec6df24fce7f80f3f85e63873651
Ethan Vander Kooi <ethrvander@gmail.com>
2018-03-14 12:50:38 -0400

    Created Settings page

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ String _bottomMessage = "BOTTOM MESSAGE";
 String _image1Url = "";
 String _image2Url = "";
 List<Period> _periods = [];
+bool _grade9Mode = false;
+bool _hideTopMessage = false;
+
+enum MenuChoices { refresh, settings }
 
 final String graphicsBaseUrl = "http://splash.tdchristian.ca/apps/infoboard/";
 
@@ -21,9 +25,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new InfoBoard(),
-    );
+        home: new InfoBoard(),
+        theme: new ThemeData(primaryColor: Colors.green));
   }
+}
+
+class SettingsPage extends StatefulWidget {
+  SettingsPage({Key key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() => new _SettingsPageState();
 }
 
 class InfoBoard extends StatefulWidget {
@@ -36,8 +47,8 @@ class InfoBoard extends StatefulWidget {
 class _InfoBoardState extends State<InfoBoard> {
 
   _getInfoBoard() async {
-
-    final String url = 'http://splash.tdchristian.ca/apps/if2/getScreenData.php';
+    final String url =
+        'http://splash.tdchristian.ca/apps/if2/getScreenData.php';
     HttpClient httpClient = new HttpClient();
 
     String topMessage;
@@ -74,7 +85,6 @@ class _InfoBoardState extends State<InfoBoard> {
   }
 
   List<Period> getPeriods(List<List<String>> periodsList) {
-
     List<Period> periods = [];
 
     if (periodsList.length > 0) {
@@ -83,93 +93,106 @@ class _InfoBoardState extends State<InfoBoard> {
       }
     }
     return periods;
-
   }
 
   @override
   Widget build(BuildContext context) {
-
     _getInfoBoard();
 
+    _topMessage = "TOP MESSAGE";
+    _bottomMessage = "BOTTOM MESSAGE";
+
     return new Container(
-      child: new Scaffold(
-        backgroundColor: Colors.green,
-        body: new ListView(
+        child: new Scaffold(
+      backgroundColor: Colors.green,
+      body: new Column(children: [
+        new Column(
+          children: _periods.map((period) => new PeriodWidget(period)).toList(),
+        ),
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            new SizedBox(
-              height: 20.0,
+            new Image.network(
+              _image1Url != ""
+                  ? graphicsBaseUrl + _image1Url
+                  : "http://splash.tdchristian.ca/apps/infoboard/graphics//HappyFace.gif",
+              height: 100.0,
             ),
-            new Container(
-              margin: const EdgeInsets.all(10.0),
-              child:  new Text(
-                _topMessage,
-                textAlign: TextAlign.center,
-                style: new TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontFamily: "RobotoCondensed",
-                  fontSize: 24.0,
-                  color: Colors.white
-                )
-              )
-            ),
-            new Column(
-              children: _periods.map( (period) => new PeriodWidget(period)).toList(),
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                new Image.network(
-                  _image1Url != "" ? graphicsBaseUrl + _image1Url : "http://splash.tdchristian.ca/apps/infoboard/graphics//HappyFace.gif",
-                  height: 100.0,
-                ),
-                new Image.network(
-                  _image2Url != "" ? graphicsBaseUrl + _image2Url : "http://splash.tdchristian.ca/apps/infoboard/graphics//HappyFace.gif",
-                  height: 100.0
-                ),
-              ],
-            ),
-            new Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: new Container(
-                decoration: new BoxDecoration(
+            new Image.network(
+                _image2Url != ""
+                    ? graphicsBaseUrl + _image2Url
+                    : "http://splash.tdchristian.ca/apps/infoboard/graphics//HappyFace.gif",
+                height: 100.0),
+          ],
+        ),
+        new Expanded(
+          child: new Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: new Container(
+              decoration: new BoxDecoration(
                   color: Colors.green[700],
-                  borderRadius: new BorderRadius.all(new Radius.circular(12.0))
-                ),
-                child: new Center(
+                  borderRadius: new BorderRadius.all(new Radius.circular(12.0))),
+              child: new Center(
                   heightFactor: 5.0,
-                  child: new Text(
-                    _bottomMessage,
-                    textAlign: TextAlign.center,
-                    style: new TextStyle(
-                      fontFamily: "RobotoCondensed",
-                      fontSize: 22.0,
-                      color: Colors.white
-                    )
-                  )
-                ),
-              ),
+                  child: new Text(_bottomMessage,
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(
+                          fontFamily: "RobotoCondensed",
+                          fontSize: 22.0,
+                          color: Colors.white))),
             ),
-          ]
+          )
+        )
+      ]),
+      floatingActionButton: new FloatingActionButton(
+        backgroundColor: Colors.green,
+        child: new Icon(Icons.refresh),
+        onPressed: _getInfoBoard,
+      ),
+      appBar: new AppBar(
+        elevation: 0.0,
+        title: new Text(
+          _topMessage,
+          textAlign: TextAlign.center,
+          style: new TextStyle(
+              fontFamily: "RobotoCondensed",
+              fontSize: 24.0,
+              fontWeight: FontWeight.w600),
         ),
-        floatingActionButton: new FloatingActionButton(
-          backgroundColor: Colors.green,
-          child: new Icon(Icons.refresh),
-          onPressed: _getInfoBoard,
-        ),
-      )
-    );
+        actions: <Widget>[
+          new PopupMenuButton<MenuChoices>(
+              onSelected: (MenuChoices result) {
+                setState(() {
+                  if (result == MenuChoices.refresh) {
+                    _getInfoBoard();
+                  } else if (result == MenuChoices.settings) {
+                    Navigator.push(
+                      context,
+                      new MaterialPageRoute(builder: (context) => new SettingsPage()));
+                  }
+                });
+              },
+              itemBuilder: (BuildContext context) =>
+                  <PopupMenuEntry<MenuChoices>>[
+                    const PopupMenuItem<MenuChoices>(
+                      value: MenuChoices.refresh,
+                      child: const Text('Refresh'),
+                    ),
+                    const PopupMenuItem<MenuChoices>(
+                        value: MenuChoices.settings,
+                        child: const Text("Settings"))
+                  ]),
+        ],
+      ),
+    ));
   }
 }
 
-
-
 class PeriodWidget extends StatelessWidget {
-  
   final Period _period;
   PeriodWidget(this._period);
 
   String change24HourTo12Hour(String time24) {
-    
     int colonIndex = time24.indexOf(':');
     int hour = int.parse(time24.substring(0, colonIndex));
     int minute = int.parse(time24.substring(colonIndex + 1));
@@ -177,44 +200,41 @@ class PeriodWidget extends StatelessWidget {
     var dateTime = new DateTime(0, 0, 0, hour, minute);
     var formatter = new DateFormat('K:m');
     String time12 = formatter.format(dateTime);
-    
+
     if (minute < 10) {
       colonIndex = time12.indexOf(":");
       String minuteString = "0" + minute.toString();
       return time12.replaceRange(colonIndex + 1, time12.length, minuteString);
-    } else { return time12; }
+    } else {
+      return time12;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-
     return new Padding(
-      padding: new EdgeInsets.all(12.0),
-      child: new Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          new Text(
-              _period.name,
-              style: new TextStyle(
-                fontWeight: FontWeight.w600,
-                fontFamily: "RobotoCondensed",
-                fontSize: 24.0,
-                color: Colors.white
-              ),
-              textAlign: TextAlign.center
-              ),
-          new Text(
-            change24HourTo12Hour(_period.startTime) + " - " + change24HourTo12Hour(_period.endTime),
-              style: new TextStyle(
-                fontWeight: FontWeight.w600,
-                fontFamily: "RobotoCondensed",
-                fontSize: 24.0,
-                color: Colors.white
-              ),
-              textAlign: TextAlign.center),
-        ]
-      )
-    );
+        padding: new EdgeInsets.all(_periods.length * 2.0),
+        child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              new Text(_period.name,
+                  style: new TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontFamily: "RobotoCondensed",
+                      fontSize: 24.0,
+                      color: Colors.white),
+                  textAlign: TextAlign.center),
+              new Text(
+                  change24HourTo12Hour(_period.startTime) +
+                      " - " +
+                      change24HourTo12Hour(_period.endTime),
+                  style: new TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontFamily: "RobotoCondensed",
+                      fontSize: 24.0,
+                      color: Colors.white),
+                  textAlign: TextAlign.center),
+            ]));
   }
 }
 
@@ -227,6 +247,60 @@ List<Period> getPeriods(List<List<String>> periodsList) {
       periods.add(new Period(p[0], p[1], p[2]));
     }
   }
-  
+
   return periods;
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    Divider divider = new Divider();
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Settings'),
+        elevation: 0.0,
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new SizedBox(
+            height: 8.0
+          ),
+          new ListTile(
+            title: new Text("Hide Day 1/Day 2 Message"),
+            trailing: new Switch(
+              value: _hideTopMessage,
+              onChanged: (bool newValue) {
+                setState(() {
+                  _hideTopMessage = newValue;
+                });
+              },
+              activeColor: Colors.green,
+            ),
+          ),
+          divider,
+          new ListTile(
+            title: new Text("Enable Grade 9 Mode"),
+            subtitle: new Padding( 
+              padding: new EdgeInsets.only(top: 3.0),
+              child: new Text("This will enable changing Day 1 & 2 classes")
+            ),
+            trailing: new Switch(
+              value: _grade9Mode,
+              onChanged: (bool newValue) {
+                setState(() {
+                  _grade9Mode = newValue;
+                });
+              },
+              activeColor: Colors.green,
+            ),
+          ),
+          divider,
+
+        ],
+      )
+    );
+  }
 }
